fd59ff3ed8dc6e345503f4a963c0922bb09f65c9
cbgrace <117283830+cbgrace@users.noreply.github.com>
2023-12-12 17:47:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+
+url = "https://api.gsa.gov/analytics/dap/v1.1"
+endpoint_one = "/reports/<report name>/data"
+endpoint_two = "/agencies/<agency name>/reports/<report name>/data"
+endpoint_three = "/domain/<domain>/reports/<report name>/data"
+
+headers = {
+	'x-api-key': 'API_KEY' }
+
+-> response = requests.get(url, headers=headers) <-
+
+"""
