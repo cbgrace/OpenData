2a3d3631e1e7efecae3dae02de02e831d6bd1d67
cbgrace <117283830+cbgrace@users.noreply.github.com>
2023-12-12 18:36:53 -0600

    x

--- dal/dal.py
+++ dal/dal.py
@@ -1,6 +1,8 @@
 import requests
 from config import API_KEY, DATABASE_PATH
 from exceptions import DalException
+from abc import ABC, abstractmethod
+
 
 """
 
@@ -18,8 +20,18 @@ Constants:
     
 """
 
-URL = "https://api.gsa.gov/analytics/dap/v1.1"
+# URL = "https://api.gsa.gov/analytics/dap/v1.1"
 REPORTS_ENDPOINT = "/reports/1/data"  # /reports/<report name>/data
 AGENCIES_ENDPOINT = "/agencies/1/reports/2/data"  # /agencies/<agency name>/reports/<report name>/data
 DOMAIN_ENDPOINT = "/domain/1/reports/2/data"  # /domain/<domain>/reports/<report name>/data
+
+class APIAdapter(ABC):
+    """
+    do I need 3 adapters (one for each endpoint) or just one?
+    """
+    URL = "https://api.gsa.gov/analytics/dap/v1.1"
+
+    @abstractmethod
+    def build_endpoint(self, *args):
+        pass
 
